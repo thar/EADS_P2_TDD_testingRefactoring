b39fb05b690c82369b2813af4735e843ed37ce87
Miguel Angel Julian <miguel.a.j82@gmail.com>
2016-08-19 16:01:34 +0200

    Tests para el analizador extendidos

---
+++ tests/TestsAnalizador.cpp
@@ -0,0 +1,45 @@
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include "Analizador.h"
+
+TEST(Analizador, whenDataConUnaNoticiaThenUnaNoticiaEsRecuperadaDelAnalizador)
+{
+    Analizador a1("dataTests");
+    auto noticias = a1.getNoticias();
+    EXPECT_EQ(noticias.size(), 1);
+    EXPECT_EQ(noticias.front().getTitulo(), " lista de palabras del alfabeto radiofónico");
+}
+
+TEST(Analizador, testConstructorSinParametros)
+{
+    Analizador a1;
+    a1.setNoticas("dataTests");
+    auto noticias = a1.getNoticias();
+    EXPECT_EQ(noticias.size(), 1);
+    EXPECT_EQ(noticias.front().getTitulo(), " lista de palabras del alfabeto radiofónico");
+}
+
+TEST(Analizador, testAgrupacionSimple)
+{
+    Analizador a1("dataTests2");
+    auto noticias = a1.getNoticias();
+    EXPECT_EQ(noticias.size(), 4);
+    EXPECT_EQ(a1.agruparNoticias(), "\nCuarta\n"
+            "*[ titulo de la Cuarta Noticia]\n"
+            "\n"
+            "Noticia\n"
+            "*[ titulo de la Tercera Noticia]\n"
+            "*[ titulo de la Segunda Noticia]\n"
+            "*[ titulo de la Primera Noticia]\n");
+}
+
+TEST(Analizador, testAgrupacionTematico)
+{
+    Analizador a1("dataTests2");
+    auto noticias = a1.getNoticias();
+    EXPECT_EQ(noticias.size(), 4);
+    EXPECT_EQ(a1.agruparNoticiasGeneral(), "[ titulo de la Cuarta Noticia]\n"
+            "   *[ titulo de la Tercera Noticia]\n"
+            "   *[ titulo de la Segunda Noticia]\n"
+            "   *[ titulo de la Primera Noticia]\n\n");
+}
