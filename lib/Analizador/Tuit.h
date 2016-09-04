e9feca0b91442e2bd29167d2f7d9036e09965821
Miguel Angel Julian <miguel.a.j82@gmail.com>
2016-09-04 20:29:36 +0200

    Refactorizar Tuit

--- lib/Analizador/Tuit.h
+++ lib/Analizador/Tuit.h
@@ -9,34 +9,40 @@
 class Tuit : public NoticiaInterface
 {
 public:
-    Tuit(Json::Value& inputData, std::shared_ptr<PalabrasReservadasInterface> palabrasReservadas) {}
+    Tuit(Json::Value& inputData, std::shared_ptr<PalabrasReservadasInterface> palabrasReservadas);
 
     void setTitulo(std::string titulo) {}
     void setCuerpo(std::string cuerpo) {}
     void setPalabrasReservadas(std::shared_ptr<PalabrasReservadasInterface> palabrasReservadas) {}
+	void inicializar();
 
-    virtual std::string getTitulo() const { return "Noticia Entidad Entidad Prueba Tuit"; }
+    virtual std::string getTitulo() const { return tuit; }
     virtual std::string getCuerpo() const { return ""; }
-    virtual EntidadComposite getEntidadMasFrecuente() const { return "Entidad"; }
-    virtual std::set<std::string> getEntidades() const {
-        std::set<std::string> s;
-        s.insert("Noticia");
-        s.insert("Entidad");
-        s.insert("Prueba");
-        s.insert("Tuit");
-        return s; }
-    virtual int getFrecuenciaEntidad(EntidadComposite entidad) const {
-        if (entidad == "Entidad")
-            return 2;
-        else
-            return 1; }
-    virtual std::shared_ptr<PalabrasReservadasInterface> getPalabrasReservadas() const { return nullptr; }
-    virtual std::set<std::string> getEntidadesRelevantes() const { return std::set<std::string>(); }
-    std::string toString() const { return  ""; };
+    virtual EntidadComposite getEntidadMasFrecuente() const { return entidadMasFrecuente; }
+    virtual std::set<std::string> getEntidades() const;
+    virtual int getFrecuenciaEntidad(EntidadComposite entidad) const;
+    virtual std::shared_ptr<PalabrasReservadasInterface> getPalabrasReservadas() const { return palabrasReservadas; }
+    virtual std::set<std::string> getEntidadesRelevantes() const { return entidadesRelevantes; }
+    std::string toString() const;
     void accept(NoticiaVisitor& visitor) {  }
+
+    std::string getUser() const { return user; }
+    int getId() const { return id; }
 
 protected:
 private:
+    void procesarEntidades();
+	void procesarEntidadMasFrecuente();
+	bool agregarEntidad(std::string nombre);
+
+    std::map<std::string, int> entidades;
+    std::set<std::string> entidadesRelevantes;
+    EntidadComposite entidadMasFrecuente;
+    std::shared_ptr<PalabrasReservadasInterface> palabrasReservadas;
+
+    std::string tuit;
+    std::string user;
+    int id;
 };
 
 
