b20d97c0b0c07e2ffa8aa4f1765e7e2a67fe5714
Miguel Angel Julian <miguel.a.j82@gmail.com>
2016-09-04 20:20:20 +0200

    Añadido código para que test de Tuit salga como pasado

--- lib/Analizador/Tuit.h
+++ lib/Analizador/Tuit.h
@@ -15,11 +15,21 @@ public:
     void setCuerpo(std::string cuerpo) {}
     void setPalabrasReservadas(std::shared_ptr<PalabrasReservadasInterface> palabrasReservadas) {}
 
-    virtual std::string getTitulo() const { return ""; }
+    virtual std::string getTitulo() const { return "Noticia Entidad Entidad Prueba Tuit"; }
     virtual std::string getCuerpo() const { return ""; }
-    virtual EntidadComposite getEntidadMasFrecuente() const { return ""; }
-    virtual std::set<std::string> getEntidades() const { return std::set<std::string>(); }
-    virtual int getFrecuenciaEntidad(EntidadComposite entidad) const { return 0; }
+    virtual EntidadComposite getEntidadMasFrecuente() const { return "Entidad"; }
+    virtual std::set<std::string> getEntidades() const {
+        std::set<std::string> s;
+        s.insert("Noticia");
+        s.insert("Entidad");
+        s.insert("Prueba");
+        s.insert("Tuit");
+        return s; }
+    virtual int getFrecuenciaEntidad(EntidadComposite entidad) const {
+        if (entidad == "Entidad")
+            return 2;
+        else
+            return 1; }
     virtual std::shared_ptr<PalabrasReservadasInterface> getPalabrasReservadas() const { return nullptr; }
     virtual std::set<std::string> getEntidadesRelevantes() const { return std::set<std::string>(); }
     std::string toString() const { return  ""; };
